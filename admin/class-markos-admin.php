a1325152b9e416deaa064c0fbbbfdaeb4a3a4ae6
Markos Muche <markosmuche2018@gmail.com>
2022-01-26 10:25:34 +0300

    Add submenu

--- admin/class-markos-admin.php
+++ admin/class-markos-admin.php
@@ -117,10 +117,27 @@ class Markos_Admin
 			'dashicons-email',
 			20
 		);
+		//add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', int $position = null )
+		add_submenu_page(
+			$this->plugin_name,
+			'Your plugin Settings',
+			'Settings',
+			'administrator',
+			$this->plugin_name . '-settings',
+			array(
+				$this,
+				'displayAdminSettings',
+			)
+		);
 	}
 
 	public function displayAdminDashboard()
 	{
 		require_once 'partials/' . $this->plugin_name . '-admin-display.php';
+	}
+
+	public function displayAdminSettings()
+	{
+		require_once 'partials/' . $this->plugin_name . '-admin-settings-display.php';
 	}
 }
